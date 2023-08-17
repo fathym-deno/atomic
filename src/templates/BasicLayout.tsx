d7e3d80a0d331d2ac333989c48abf3ff43888f6b
mcgear <michael.gearhardt@gmail.com>
2023-08-17 10:03:58 -0600

    Simplify basic layout

--- src/templates/BasicLayout.tsx
+++ src/templates/BasicLayout.tsx
@@ -7,35 +7,35 @@ import { Header, HeaderProps } from "../organisms/Header.tsx";
 import { Footer, FooterProps } from "../organisms/Footer.tsx";
 
 export interface BasicLayoutProps extends JSX.HTMLAttributes<HTMLDivElement> {
-  footer?: FooterProps | ComponentChildren;
+  footer?: ComponentChildren;
 
-  header?: HeaderProps | ComponentChildren;
+  header?: ComponentChildren;
 
   children?: ComponentChildren;
 }
 
 export function BasicLayout(props: BasicLayoutProps) {
-  const { headerProps, header } = {
-    headerProps: (props.header as HeaderProps)?.nav
-      ? props.header as HeaderProps
-      : undefined,
-    header: !(props.header as HeaderProps)?.nav
-      ? props.header as ComponentChildren
-      : undefined,
-  };
+  // const { headerProps, header } = {
+  //   headerProps: (props.header as HeaderProps)?.nav
+  //     ? props.header as HeaderProps
+  //     : undefined,
+  //   header: !(props.header as HeaderProps)?.nav
+  //     ? props.header as ComponentChildren
+  //     : undefined,
+  // };
 
-  const { footerProps, footer } = {
-    footerProps: (props.footer as FooterProps)?.nav
-      ? props.footer as FooterProps
-      : undefined,
-    footer: !(props.footer as FooterProps)?.nav
-      ? props.footer as ComponentChildren
-      : undefined,
-  };
+  // const { footerProps, footer } = {
+  //   footerProps: (props.footer as FooterProps)?.nav
+  //     ? props.footer as FooterProps
+  //     : undefined,
+  //   footer: !(props.footer as FooterProps)?.nav
+  //     ? props.footer as ComponentChildren
+  //     : undefined,
+  // };
 
   return (
     <>
-      {headerProps ? <Header {...headerProps} /> : header}
+      {props.header}
 
       {props.children && (
         <main class="flex-grow" style={{ minHeight: "calc(100vh - 15vh)" }}>
@@ -43,7 +43,7 @@ export function BasicLayout(props: BasicLayoutProps) {
         </main>
       )}
 
-      {footerProps ? <Footer {...footerProps} /> : footer}
+      {props.footer}
     </>
   );
 }
--- tests/templates/BasicLayout.tests.tsx
+++ tests/templates/BasicLayout.tests.tsx
@@ -17,7 +17,7 @@ import {
 } from "../test.deps.ts";
 
 describe("Basic Layout Tests", () => {
-  describe("With Props", () => {
+  describe("With Components", () => {
     const logo = {
       LogoAlt: "Fathym Open BioTech",
       LogoUrl: "/o-biotech-logo.svg",
@@ -28,65 +28,6 @@ describe("Basic Layout Tests", () => {
       <Header
         title="Hello World"
         logo={logo}
-        nav={
-          <>
-            <Action href="/" class="text-xl mx-1">
-              Home
-            </Action>
-
-            <Action href="/about" class="text-xl mx-1">
-              About
-            </Action>
-
-            <Action href="/contact" class="text-xl mx-1">
-              Contact
-            </Action>
-          </>
-        }
-      />
-    );
-
-    const footer = (
-      <Footer
-        companyName="Fathym Test"
-        companyDescription="This is a description of the test"
-        nav={[{
-          class: "text-xl mx-1",
-          href: "/",
-          children: "Home",
-        }, {
-          class: "text-xl mx-1",
-          href: "/about",
-          children: "About",
-        }, {
-          class: "text-xl mx-1",
-          href: "/contact",
-          children: "Contact",
-        }]}
-      />
-    );
-
-    const html = render(
-      <BasicLayout header={header.props} footer={footer.props}>
-        <>
-          <h2>This is the main content.</h2>
-        </>
-      </BasicLayout>,
-    );
-
-    console.log(html);
-
-    assert(html.includes(`main class="flex-grow"`));
-    assert(html.includes(`<h2>This is the main content.</h2>`));
-    assert(html.includes("This is a description of the test"));
-    assert(html.includes("Hello World"));
-    assert(html.includes("/o-biotech-logo.svg"));
-  });
-
-  describe("With Components", () => {
-    const header = (
-      <Header
-        logo={<Action href="/">Hello World</Action>}
         nav={
           <>
             <Action href="/" class="text-xl mx-1">
@@ -137,5 +78,6 @@ describe("Basic Layout Tests", () => {
     assert(html.includes(`<h2>This is the main content.</h2>`));
     assert(html.includes("This is a description of the test"));
     assert(html.includes("Hello World"));
+    assert(html.includes("/o-biotech-logo.svg"));
   });
 });
