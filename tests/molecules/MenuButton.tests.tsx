2448283473f31eda08e11797b51d6dd9f21403a5
mcgear <michael.gearhardt@gmail.com>
2023-08-19 16:36:54 -0600

    Cleanup

--- tests/molecules/MenuButton.tests.tsx
+++ tests/molecules/MenuButton.tests.tsx
@@ -1,15 +1,5 @@
-import {
-  afterEach,
-  assert,
-  assertEquals,
-  beforeEach,
-  describe,
-  it,
-  render,
-} from "../test.deps.ts";
+import { assert, describe, render } from "../test.deps.ts";
 
-import { Action } from "../../src/atoms/Action.tsx";
-import { ActionGroup } from "../../src/molecules/ActionGroup.tsx";
 import {
   MenuButton,
   MenuButtonStyleTypes,
