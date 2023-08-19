21874caa277a3991c99d39e797fb0b5d5b6d1eae
mcgear <michael.gearhardt@gmail.com>
2023-08-19 16:42:37 -0600

    upgraded atomic icons

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -3,4 +3,4 @@ export {
   type JSX,
 } from "https://esm.sh/preact@10.16.0";
 export { useEffect, useState } from "https://esm.sh/preact@10.16.0/hooks";
-export * from "https://deno.land/x/fathym_atomic_icons@v0.0.3-integration1/mod.ts";
+export * from "https://deno.land/x/fathym_atomic_icons@v0.0.3-integration17/mod.ts";
