78bc0f19c80f0ce38b01bd6058ddae9ae49c0eda
mcgear <michael.gearhardt@gmail.com>
2023-08-22 10:52:17 -0600

    input tests working

--- tests/atoms/forms/Input.tests.tsx
+++ tests/atoms/forms/Input.tests.tsx
@@ -1,37 +1,20 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import Input from '../../../src/atoms/forms/Input';
-
-import { afterEach, assert, assertEquals, beforeEach, describe, it, render, } from "../../test.deps.ts";
+import {
+  afterEach,
+  assert,
+  assertEquals,
+  beforeEach,
+  describe,
+  it,
+  render,
+} from "../../test.deps.ts";
 import { Input } from "../../../src/atoms/forms/Input.tsx";
 
-describe('Input Tests', () => {
-  it('Renders input element', () => {
-    const { getByTestId } = render(<Input />);
-    assert(getByTestId('input'));
-  });
-
-  it('Input value changes when typing', () => {
-    const { getByTestId } = render(<Input />);
-    const inputElement = getByTestId('input');
-
-    inputElement.value = 'test value';
-    inputElement.dispatchEvent(new Event('input'));
-    assertEquals(inputElement.value, 'test value');
-  });
-});
-
-describe('Input component', () => {
-  test('renders input element', () => {
-    const { getByTestId } = render(<Input />);
-    expect(getByTestId('input')).toBeInTheDocument();
-  });
-
-  test('input value changes when typing', () => {
-    const { getByTestId } = render(<Input />);
-    const inputElement = getByTestId('input');
-
-    fireEvent.change(inputElement, { target: { value: 'test value' } });
-    expect(inputElement.value).toBe('test value');
+describe("Input Tests", () => {
+  it("Renders input element", () => {
+    const html = render(<Input />);
+    assertEquals(
+      html,
+      `<input type="text" class="w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>`,
+    );
   });
 });
