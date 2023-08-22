3eaba71f20e744b2d0e43c52802e4b2ed791aab1
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-22 16:47:18 +0000

    Refactor Input.tests.tsx to import dependencies and add tests for rendering input element and changing input value.

--- tests/atoms/forms/Input.tests.tsx
+++ tests/atoms/forms/Input.tests.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Input from '../../../src/atoms/forms/Input';
 
+import { afterEach, assert, assertEquals, beforeEach, describe, it, render, } from "../../test.deps.ts";
+import { Input } from "../../../src/atoms/forms/Input.tsx";
+
+describe('Input Tests', () => {
+  it('Renders input element', () => {
+    const { getByTestId } = render(<Input />);
+    assert(getByTestId('input'));
+  });
+
+  it('Input value changes when typing', () => {
+    const { getByTestId } = render(<Input />);
+    const inputElement = getByTestId('input');
+
+    inputElement.value = 'test value';
+    inputElement.dispatchEvent(new Event('input'));
+    assertEquals(inputElement.value, 'test value');
+  });
+});
+
 describe('Input component', () => {
   test('renders input element', () => {
     const { getByTestId } = render(<Input />);
