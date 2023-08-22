e31ed54b79c73f295f965df6b3f6ad55cdc17463
mcgear <michael.gearhardt@gmail.com>
2023-08-22 10:35:33 -0600

    Integrated tests

--- tests/atoms/forms/SlideToggle.tests.tsx
+++ tests/atoms/forms/SlideToggle.tests.tsx
@@ -6,7 +6,7 @@ import {
   describe,
   it,
   render,
-} from "../test.deps.ts";
+} from "../../test.deps.ts";
 
 import { SlideToggle } from "../../../src/atoms/forms/SlideToggle.tsx";
 
@@ -15,7 +15,7 @@ describe("SlideToggle Tests", () => {
     const html = render(<SlideToggle checked={true} value="toggle" />);
     assertEquals(
       html,
-      `<label class="relative inline-flex items-center cursor-pointer"><input onChange={() => setCheckedState(!checked)} value="toggle" checked type="checkbox" class="sr-only peer"><div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div></label>`
+      `<label class="relative inline-flex items-center cursor-pointer"><input value="toggle" checked type="checkbox" class="sr-only peer"/><div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div></label>`,
     );
   });
 });
