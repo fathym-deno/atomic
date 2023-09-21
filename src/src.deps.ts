e21a1d559419dc915f3345627ca70dd3192afc41
mcgear <michael.gearhardt@gmail.com>
2023-09-21 15:45:41 -0600

    adjust version and removed fresh

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,6 +1,2 @@
-export {
-  type ComponentChildren,
-  type JSX,
-} from "https://esm.sh/preact@10.16.0";
+export * from "https://esm.sh/preact@10.16.0";
 export { useEffect, useState } from "https://esm.sh/preact@10.16.0/hooks";
-export * from "https://deno.land/x/fathym_atomic_icons@v0.0.3-integration17/mod.ts";
