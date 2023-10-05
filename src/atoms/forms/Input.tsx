7c0f23ef30b83ca0eb34a26f2a0dafae927b17eb
mcgear <michael.gearhardt@gmail.com>
2023-10-05 10:33:46 -0600

    add text area to input control

--- src/atoms/forms/Input.tsx
+++ src/atoms/forms/Input.tsx
@@ -1,35 +1,69 @@
 import { ComponentChildren, type JSX, useState } from "../../src.deps.ts";
 import { classSet } from "../../utils/jsx.utils.tsx";
 
-export type InputProps = JSX.HTMLAttributes<HTMLInputElement> & {
-  prepareValue?: (value: string) => string;
-};
+export type InputProps =
+  & (
+    | JSX.HTMLAttributes<HTMLInputElement>
+    | JSX.HTMLAttributes<HTMLTextAreaElement>
+  )
+  & {
+    multiline?: boolean;
+
+    prepareValue?: (value: string) => string;
+  };
 
 export function Input(props: InputProps) {
   const { prepareValue, value, ...rest } = props;
 
   const [valueState, setValue] = useState(value);
 
-  return (
-    <input
-      onChange={(e) => {
-        const target = (e.target as HTMLInputElement)!;
+  const onValueChange = (value: string) => {
+    if (prepareValue) {
+      value = prepareValue(value);
+    }
 
-        let value = target.value;
+    setValue(value);
+  };
 
-        if (prepareValue) {
-          value = prepareValue(value);
-        }
+  const onTextAreaChange = (
+    e: JSX.TargetedEvent<HTMLTextAreaElement, Event>,
+  ) => {
+    const target = (e.target as HTMLTextAreaElement)!;
 
-        setValue(value);
-      }}
-      {...rest}
-      value={valueState}
-      type={props.type || "text"}
-      class={classSet(
-        props,
-        "w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500",
-      )}
-    />
-  );
+    onValueChange(target.value);
+  };
+
+  const onInputChange = (e: JSX.TargetedEvent<HTMLInputElement, Event>) => {
+    const target = (e.target as HTMLInputElement)!;
+
+    onValueChange(target.value);
+  };
+
+  const input = props.multiline
+    ? (
+      <textarea
+        onChange={onTextAreaChange}
+        {...(rest as JSX.HTMLAttributes<HTMLTextAreaElement>)}
+        value={valueState}
+        class={classSet(
+          props,
+          "w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500",
+        )}
+      >
+      </textarea>
+    )
+    : (
+      <input
+        onChange={onInputChange}
+        {...(rest as JSX.HTMLAttributes<HTMLInputElement>)}
+        value={valueState}
+        type={props.type || "text"}
+        class={classSet(
+          props,
+          "px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500",
+        )}
+      />
+    );
+
+  return input;
 }
--- tests/atoms/forms/Input.tests.tsx
+++ tests/atoms/forms/Input.tests.tsx
@@ -14,7 +14,7 @@ describe("Input Tests", () => {
     const html = render(<Input />);
     assertEquals(
       html,
-      `<input type="text" class="w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>`,
+      `<input type="text" class="px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>`,
     );
   });
 });
