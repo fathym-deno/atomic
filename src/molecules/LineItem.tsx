b9f1b85597dc6d5193aca78441bb01c3908ec8b4
mcgear <michael.gearhardt@gmail.com>
2023-10-10 12:34:06 -0600

    LineItem

---
+++ src/molecules/LineItem.tsx
@@ -0,0 +1,66 @@
+import { ComponentChildren, JSX, useEffect, useState } from "../src.deps.ts";
+import { classSet } from "../utils/jsx.utils.tsx";
+
+export type LineItemProps = Omit<JSX.HTMLAttributes<HTMLDivElement>, "icon"> & {
+  actionPath: string;
+
+  actionText: ComponentChildren;
+
+  confirmAction?: () => void;
+
+  confirmIcon?: ComponentChildren;
+
+  confirmRedirect?: string;
+
+  confirmText?: string;
+};
+
+export function LineItem(props: LineItemProps) {
+  const [reload, setReload] = useState(false);
+
+  const {
+    actionText,
+    actionPath,
+    confirmAction,
+    confirmIcon,
+    confirmRedirect,
+    confirmText,
+  } = props;
+
+  const handleAction = async () => {
+    if (confirmAction) {
+      if (confirmText && window.confirm(confirmText)) {
+        await confirmAction();
+      } else {
+        await confirmAction();
+      }
+
+      setReload(true);
+    }
+  };
+
+  useEffect(() => {
+    if (reload) {
+      location.href = confirmRedirect || `${location.href}`;
+    }
+  }, [reload]);
+
+  return (
+    <div>
+      <a href={actionPath} class="text-blue-500 hover:underline">
+        {actionText}
+      </a>
+
+      {confirmIcon && (
+        <button
+          class="ml-2"
+          onClick={() => {
+            handleAction().then();
+          }}
+        >
+          {confirmIcon}
+        </button>
+      )}
+    </div>
+  );
+}
--- src/molecules/_exports.tsx
+++ src/molecules/_exports.tsx
@@ -1,5 +1,6 @@
 export * from "./ActionGroup.tsx";
 export * from "./Display.tsx";
+export * from "./LineItem.tsx";
 export * from "./MenuButton.tsx";
 export * from "./ResponsiveSet.tsx";
 export * from "./Tabs.tsx";
