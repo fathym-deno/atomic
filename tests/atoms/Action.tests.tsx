917410e1347ffaa65a29f76dc9f697910f8cb556
mcgear <michael.gearhardt@gmail.com>
2024-01-08 12:28:29 -0700

    upgrades

--- tests/atoms/Action.tests.tsx
+++ tests/atoms/Action.tests.tsx
@@ -29,6 +29,15 @@ describe("Action Tests", () => {
     );
   });
 
+  describe("Class Overrides", () => {
+    const html = render(<Action class="px-0 py-0">Hello</Action>);
+
+    assertEquals(
+      html,
+      `<button class="block px-4 py-2 font-bold transition-colors duration-200 ease-out rounded bg-blue-500 text-white border-none hover:(bg-blue-700 bg-opacity-80 text-white) px-0 py-0">Hello</button>`,
+    );
+  });
+
   describe("Action Styles: None", () => {
     const html = render(
       <Action actionStyle={ActionStyleTypes.None}>Hello</Action>,
