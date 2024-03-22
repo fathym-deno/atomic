94d3ac72d8272e19411284fd078cf902376527b4
mcgear <michael.gearhardt@gmail.com>
2024-03-22 16:25:16 -0600

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -9,5 +9,4 @@ export {
   forwardRef,
 } from "https://esm.sh/preact@10.19.2/compat";
 export { useEffect, useState } from "https://esm.sh/preact@10.19.2/hooks";
-
 export * from "https://deno.land/x/fathym_common@v0.0.177/mod.ts";
