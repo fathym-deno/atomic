b8249411199ee89218ab4c897758418b9b85d1f3
mcgear <michael.gearhardt@gmail.com>
2024-03-24 16:11:00 -0600

    ref update

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -3,10 +3,10 @@ export {
   isValidElement,
   type JSX,
   type VNode,
-} from "https://esm.sh/preact@10.19.2";
+} from "https://esm.sh/preact@10.19.6";
 export {
   type ForwardedRef,
   forwardRef,
-} from "https://esm.sh/preact@10.19.2/compat";
-export { useEffect, useState } from "https://esm.sh/preact@10.19.2/hooks";
+} from "https://esm.sh/preact@10.19.6/compat";
+export { useEffect, useState } from "https://esm.sh/preact@10.19.6/hooks";
 export * from "https://deno.land/x/fathym_common@v0.0.177/mod.ts";
