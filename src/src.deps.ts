ee7bb8962b249fec50843009cf2facf1d5f96a61
mcgear <michael.gearhardt@gmail.com>
2024-08-20 15:06:32 -0600

    ref update

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -13,4 +13,4 @@ export { useEffect, useState } from "npm:preact@10.20.1/hooks";
 export * from "jsr:@fathym/common@0.2.22";
 
 // export { Icon } from '../../atomic-icons/browser.ts';
-export { Icon } from "jsr:@fathym/atomic-icons@0.0.58/browser";
+export { Icon } from "jsr:@fathym/atomic-icons@0.0.59/browser";
